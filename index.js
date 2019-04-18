6f5c24a327259b1351b165e9edf742628294daa2
Jacob BLaylock <jacobblaylock@gmail.com>
2019-04-18 17:10:19 -0600

    Revert changes

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 const express = require('express')
-const passport = require('passport')
-const OAuth2Strategy = require('passport-oauth2')
-// const keys = require('./config/keys')
-const keys = {
-    helpscoutClientID: '94080494c1464f2c8f9b9d39adfacce2',
-    helpscoutClientSecret: '264424344f514ea8be89e1cca7353076'
-}
-
 const app = express()
 
-passport.use(new OAuth2Strategy({
-        authorizationURL: 'https://secure.helpscout.net/authentication/authorizeClientApplication',
-        tokenURL: 'https://api.helpscout.net/v2/oauth2/token',
-        clientID: keys.helpscoutClientID,
-        clientSecret: keys.helpscoutClientSecret,
-        callbackURL: "/auth/callback"
-    }),
-    accessToken => {
-        console.log(accessToken)
-    }
-)
-
-app.get('/auth', passport.authenticate('oauth2'))
+app.get('/', (req, res) => {
+    res.send({ hi: 'there'})
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
