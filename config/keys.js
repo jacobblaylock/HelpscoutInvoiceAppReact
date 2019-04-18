02ab3d53e88837966e13c35ccbe90ad65d18f5ec
Jacob BLaylock <jacobblaylock@gmail.com>
2019-04-18 17:12:06 -0600

    Remove config folder

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    helpscoutClientID: '94080494c1464f2c8f9b9d39adfacce2',
-    helpscoutClientSecret: '264424344f514ea8be89e1cca7353076'
-}
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const passport = require('passport')
+const OAuth2Strategy = require('passport-oauth2')
+const keys = {
+    helpscoutClientID: '94080494c1464f2c8f9b9d39adfacce2',
+    helpscoutClientSecret: '264424344f514ea8be89e1cca7353076'
+}
+
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'})
-})
+passport.use(new OAuth2Strategy({
+        authorizationURL: 'https://secure.helpscout.net/authentication/authorizeClientApplication',
+        tokenURL: 'https://api.helpscout.net/v2/oauth2/token',
+        clientID: keys.helpscoutClientID,
+        clientSecret: keys.helpscoutClientSecret,
+        callbackURL: "/auth/callback"
+    }),
+    accessToken => {
+        console.log(accessToken)
+    }
+)
+
+app.get('/auth', passport.authenticate('oauth2'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
