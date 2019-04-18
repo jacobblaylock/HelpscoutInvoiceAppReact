b6167e407e9baf9cf8206153f98cbb1b2e739a85
Jacob BLaylock <jacobblaylock@gmail.com>
2019-04-18 16:57:39 -0600

    setup passport and OAuth2

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const passport = require('passport')
+const OAuth2Strategy = require('passport-oauth2')
+const keys = require('./config/keys')
+
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'hello world'})
-})
+passport.use(new OAuth2Strategy({
+        authorizationURL: 'https://secure.helpscout.net/authentication/authorizeClientApplication',
+        tokenURL: 'https://api.helpscout.net/v2/oauth2/token',
+        clientID: keys.helpscoutClientID,
+        clientSecret: keys.helpscoutClientSecret,
+        callbackURL: "/auth/callback"
+    }),
+    accessToken => {
+        console.log(accessToken)
+    }
+)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
