8e9f86942a49c8a4e9d618c43b27e49ba649b3df
Jacob BLaylock <jacobblaylock@gmail.com>
2019-04-19 10:38:20 -0600

    Update root router

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ passport.use(new OAuth2Strategy({
     }
 ))
 
-app.get('/', () => {hi: 'Testing OAuth2 connection to Helpscout'})
+app.get('/', () => res.send({hi: 'testing OAuth2 with Helpscout...Good Luck!'}))
 
 app.get('/auth/example', passport.authenticate('oauth2'))
 
