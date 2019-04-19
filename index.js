ff04e8fa70759d8eb5e30c84388a77d55ce69a8d
Jacob BLaylock <jacobblaylock@gmail.com>
2019-04-19 16:50:02 -0600

    Add axios and connect to mailbox endpoint

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const express = require('express')
 const passport = require('passport')
 const OAuth2Strategy = require('passport-oauth2')
+const axios = require('axios')
+
 const keys = {
     helpscoutClientID: '94080494c1464f2c8f9b9d39adfacce2',
     helpscoutClientSecret: '264424344f514ea8be89e1cca7353076'
 }
-const CircularJSON = require('circular-json')
 
 const app = express()
+
+var helpscoutConfig
 
 passport.use(new OAuth2Strategy({
         authorizationURL: 'https://secure.helpscout.net/authentication/authorizeClientApplication',
         tokenURL: 'https://api.helpscout.net/v2/oauth2/token',
         clientID: keys.helpscoutClientID,
         clientSecret: keys.helpscoutClientSecret,        
-        callbackURL: "https://mighty-journey-28056.herokuapp.com/auth/example/callback"
-        // callbackURL: "http://127.0.0.1:5000/auth/example/callback"
+        // callbackURL: "https://mighty-journey-28056.herokuapp.com/auth/example/callback"
+        callbackURL: "http://127.0.0.1:5000/auth/example/callback"
     },
     accessToken => {
-        console.log('Hello')
+        helpscoutConfig = {
+            baseURL: 'https://api.helpscout.net/v2/',
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        }
+        console.log(helpscoutConfig)
     }
 ))
 
@@ -27,14 +36,19 @@ app.get('/', (req, res) => res.send({hi: 'testing OAuth2 with Helpscout ... Good
 app.get('/auth/example', passport.authenticate('oauth2'))
 
 app.get('/auth/example/callback',
-  passport.authenticate('oauth2')
+  passport.authenticate('oauth2'),
+  (req, res) => res.redirect('/mailbox')
 )
-// app.get('/auth/example/callback', (req, res) => {
-//     // console.log(req)
-//     res.send({code: req.query.code})
-// })
 
-  app.get('/login', (req, res) => res.send({login: 'login'}))
+app.get('/login', (req, res) => res.send({login: 'login'}))
+
+app.get('/mailbox', (req, res) => {
+    axios.get('/mailboxes', helpscoutConfig)
+        .then(response => {
+            console.log(JSON.stringify(response.data))
+        })
+        .catch(error => console.log('error'))
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
