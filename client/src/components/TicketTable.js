c14131fa05729e1b7f8eb609628881c9a6f674ef
Jacob BLaylock <jacobblaylock@gmail.com>
2019-05-30 02:53:35 -0600

    Add totals to TicketTable

--- client/src/components/TicketTable.js
+++ client/src/components/TicketTable.js
@@ -9,14 +9,14 @@ import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 
 const styles = theme => ({
-    root: {
-        width: '100%',
-        marginTop: theme.spacing.unit * 3,
-        overflowX: 'auto',
-    },
-    table: {
-        minWidth: 700,
-    },
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
 })
 
 const billableHoursId = 1240
@@ -24,60 +24,71 @@ const billableHoursId = 1240
 class SimpleTable extends Component {
 
 
-    render() {
-        const { classes, stats } = this.props
+  render() {
+    const { classes, stats, totals } = this.props
 
-        return (
-            <div>
-                <h2>Ticket Snapshot</h2>
-                <Table className={classes.table}>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>User</TableCell>
-                            <TableCell>Tickets</TableCell>
-                            <TableCell>Billable Hours</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {stats.map(s => (
-                            <TableRow key={s.user}>
-                                <TableCell>{s.user}</TableCell>
-                                <TableCell>{s.tickets}</TableCell>
-                                <TableCell>{s.hours}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <h2>Ticket Snapshot</h2>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>User</TableCell>
+              <TableCell>Tickets</TableCell>
+              <TableCell>Billable Hours</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {stats.map(s => (
+              <TableRow key={s.user}>
+                <TableCell>{s.user}</TableCell>
+                <TableCell>{s.tickets}</TableCell>
+                <TableCell>{s.hours}</TableCell>
+              </TableRow>
+            ))}
+            <TableRow>
+              <TableCell>TOTALS</TableCell>
+              <TableCell>{totals.tickets}</TableCell>
+              <TableCell>{totals.hours}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    )
+  }
 }
 
 SimpleTable.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 }
 
 function mapStateToProps({ helpscout }, ownProps) {
-    return {
-        stats: helpscout.conversations.reduce((acc, cur) => {
-            let user = cur.assignee ? cur.assignee.first + ' ' + cur.assignee.last : 'unassigned'
-            let i = acc.findIndex(a => a.user === user)
-            let hours = cur.customFields.find(f => f.id === billableHoursId) ? cur.customFields.find(f => f.id === billableHoursId).value : 0
-            if (hours === '') hours = 0
+  const stats = helpscout.conversations.reduce((acc, cur) => {
+    let user = cur.assignee ? cur.assignee.first + ' ' + cur.assignee.last : 'unassigned'
+    let i = acc.findIndex(a => a.user === user)
+    let hours = cur.customFields.find(f => f.id === billableHoursId) ? cur.customFields.find(f => f.id === billableHoursId).value : 0
+    if (hours === '') hours = 0
 
-            if (i > -1) {
-                acc[i].tickets = acc[i].tickets + 1
-                acc[i].hours = (parseFloat(acc[i].hours) + parseFloat(hours)).toFixed(2)
-            } else {
-                acc.push({
-                    user,
-                    tickets: 1,
-                    hours: parseFloat(hours).toFixed(2)
-                })
-            }
-            return acc
-        }, [])
+    if (i > -1) {
+      acc[i].tickets = acc[i].tickets + 1
+      acc[i].hours = (parseFloat(acc[i].hours) + parseFloat(hours)).toFixed(2)
+    } else {
+      acc.push({
+        user,
+        tickets: 1,
+        hours: parseFloat(hours).toFixed(2)
+      })
     }
+    return acc
+  }, [])
+
+  const totals = stats.reduce((acc, cur) => {
+    return { tickets: acc.tickets + cur.tickets, hours: (parseFloat(acc.hours) + parseFloat(cur.hours)).toFixed(2)}
+  }, { tickets: 0, hours: 0 })
+  return {
+    stats,
+    totals
+  }
 }
 
 export default connect(mapStateToProps)(withStyles(styles)(SimpleTable))
