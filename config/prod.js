9d3331bc2346daed4fb5e39e723c165a88edf090
Jacob BLaylock <jacobblaylock@gmail.com>
2019-07-01 14:44:10 -0600

    Test removing spread operator

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  helpscoutClientID: 'e5e144b3a31e4b2eb11b08b8b7f25f2f',
-  helpscoutClientSecret: '199567aadcd74f518a7ebb154ea684f3'
+  helpscoutClientID: process.env.HELPSCOUT_CLIENT_ID,
+  helpscoutClientSecret: process.env.HELPSCOUT_CLIENT_SECRET
 }
